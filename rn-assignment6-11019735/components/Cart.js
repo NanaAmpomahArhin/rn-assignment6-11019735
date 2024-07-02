c20a3fa7f34ec25f87c22f27244e6d5456377047
Nana Ampomah Arhin <anarhin002@st.ug.edu.gh>
2024-07-02 13:15:09 -0700

    created a cart component

---
+++ rn-assignment6-11019735/components/Cart.js
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+
+export default function Cart() {
+  return (
+    <View>
+      <Header />
+      <HeroSection />
+    </View>
+  );
+}
