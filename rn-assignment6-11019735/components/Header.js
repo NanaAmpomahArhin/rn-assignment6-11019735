cd42ba078627bd1a48f3eee64f439e793a1f9f74
Nana Ampomah Arhin <anarhin002@st.ug.edu.gh>
2024-07-02 12:31:07 -0700

    created the header component

--- rn-assignment6-11019735/components/Header.js
+++ rn-assignment6-11019735/components/Header.js
@@ -1,1 +1,49 @@
-export default function Header() {}
+import { View, Image, StyleSheet } from "react-native";
+
+export default function Header() {
+  return (
+    <View style={styles.wrapper}>
+      <Image source={require("../assets/Menu.png")} style={styles.menu} />
+      <Image source={require("../assets/Logo.png")} style={styles.logo} />
+      <View style={styles.searchAndBagContainer}>
+        <Image source={require("../assets/Search.png")} style={styles.search} />
+        <Image
+          source={require("../assets/shoppingBag.png")}
+          style={styles.shopping}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    top: 40,
+  },
+  menu: {
+    width: 30,
+    height: 30,
+    left: 20,
+  },
+  logo: {
+    width: 100,
+    height: 40,
+    left: 15,
+  },
+  searchAndBagContainer: {
+    flexDirection: "row",
+    width: 70,
+    justifyContent: "space-between",
+    right: 20,
+  },
+  search: {
+    width: 30,
+    height: 30,
+  },
+  shopping: {
+    width: 30,
+    height: 30,
+  },
+});
