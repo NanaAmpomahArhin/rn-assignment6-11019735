540593b7f13d26c6e71045968de11537f4faf2d1
Nana Ampomah Arhin <anarhin002@st.ug.edu.gh>
2024-07-02 21:15:30 -0700

    added navigation using the stack navigator

--- rn-assignment6-11019735/App.js
+++ rn-assignment6-11019735/App.js
@@ -1,10 +1,23 @@
-import { View } from "react-native";
-import Cart from "./components/Cart";
+import HomeScreen from "./components/HomeScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import CartProvider from "./components/CartContext";
+import CartScreen from "./components/CartScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      <Cart />
-    </View>
+    <CartProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Cart" component={CartScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
   );
 }
